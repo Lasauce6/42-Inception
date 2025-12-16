d730ec9e499495308a62b0628dcf6a7d63fb982d
rbaticle <rbaticle@student.42.fr>
2025-12-16 13:01:25 +0100

    mariadb still cant connect

--- Makefile
+++ Makefile
@@ -6,20 +6,25 @@
 #    By: rbaticle <rbaticle@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/27 14:40:16 by rbaticle          #+#    #+#              #
-#    Updated: 2025/12/15 15:28:07 by rbaticle         ###   ########.fr        #
+#    Updated: 2025/12/16 12:46:33 by rbaticle         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 DC = docker compose
 
 DC_FILE = srcs/docker-compose.yml
 
-up: build
+DATA = ~/data/ ~/data/wordpress/ ~/data/mariadb/
+
+up: $(DATA)
 	$(DC) -f $(DC_FILE) up -d
 
-debug: build
+debug: $(DATA)
 	$(DC) -f $(DC_FILE) up
 
-build:
+$(DATA):
+	mkdir $(DATA)
+
+build: $(DATA)
 	$(DC) -f $(DC_FILE) build
 
 stop:
@@ -31,3 +36,9 @@ ps:
 clean:
 	$(DC) -f $(DC_FILE) down -v
 
+fclean: clean
+	sudo rm -fr ~/data/mariadb/*
+	sudo rm -fr ~/data/wordpress/*
+
+re: fclean $(DATA)
+	$(DC) -f $(DC_FILE) up -d
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
+set -e
 
-service mysql start
+# Start MariaDB in background
+mysqld_safe &
 
-mysql -e "CREATE DATABASE IF NOT EXIST \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXIST \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-kill $(cat /var/run/mysqld/mysqld.pid)
+# Wait for MariaDB to be ready
+until mysqladmin ping --silent; do
+	sleep 2
+done
 
-mysqld
+# Init database and user
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
+
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+
+FLUSH PRIVILEGES;
+EOF
+
+# Stop background mysqld_safe
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+# Start MariaDB in foreground (PID 1)
+exec mysqld
