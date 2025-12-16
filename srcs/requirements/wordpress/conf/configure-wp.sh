6b6071a725966dad0cc03622fb9c28e613fc7fae
rbaticle <rbaticle@student.42.fr>
2025-12-16 12:12:56 +0100

    mariadb working but errors connecting to it

--- srcs/requirements/wordpress/conf/configure-wp.sh
+++ srcs/requirements/wordpress/conf/configure-wp.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sleep 10
+until mysqladmin ping -h mariadb -u$SQL_USER -p$SQL_PASSWORD --silent; do
+	echo "Waiting for MariaDB..."
+	sleep 2
+done
 
 wp config create --allow-root \
 	--dbname=$SQL_DATABASE \
@@ -10,12 +13,16 @@ wp config create --allow-root \
 	--path=/var/www/wordpress
 
 if wp core is-installed 2>/dev/null; then
-	wp core install --url=$WP_URL \
+	wp core install --allow-root \
+		--url=$WP_URL \
 		--title=$WP_TITLE \
 		--admin_user=$WP_ADMIN_USER \
 		--admin_password=$WP_ADMIN_PASSWORD \
 		--admin_email=$WP_ADMIN_EMAIL \
 		--skip-email
-	wp user create $WP_USER $WP_USER_EMAIL \
-		--user_pass=$WP_USER_PASSWORD \
+			wp user create --allow-root \
+				$WP_USER $WP_USER_EMAIL \
+				--user_pass=$WP_USER_PASSWORD
 fi
+
+exec "$@"
